19cd26784afbd96064e73b7bcb5ca87172044043
CansoKing <cansoking@hotmail.com>
2022-04-06 22:12:13 +0800

    分支测试

--- src/com/archie/spring5/TestGit.java
+++ src/com/archie/spring5/TestGit.java
@@ -3,5 +3,6 @@ package com.archie.spring5;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Git版本更新测试");
+        System.out.println("分支测试");
     }
 }
