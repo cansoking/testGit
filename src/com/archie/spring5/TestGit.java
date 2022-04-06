e9d6e241a14d7469a1400ca141e90dc8a362498d
CansoKing <cansoking@hotmail.com>
2022-04-06 22:15:17 +0800

    主干添加

--- src/com/archie/spring5/TestGit.java
+++ src/com/archie/spring5/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("Git版本更新测试");
         System.out.println("分支测试");
+        System.out.println("主干添加");
     }
 }
